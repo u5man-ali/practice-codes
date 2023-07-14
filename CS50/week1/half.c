f99112ed15cd2255754ea46f8b3f43cd93425826
u5man-ali <s_usman_ali@outlook.com>
2023-07-14 12:10:47 +0500

    my first commit

---
+++ CS50/week1/half.c
@@ -0,0 +1,8 @@
+float half(float bill, float tax, float tip);
+
+int main(void)
+{
+    float bill_amount = get_float("Bill before tax and tip: ");
+    float tax_percent = get_float("Sales Tax Percent: ");
+    int tip_percent = get_int("Tip percent: ");
+}

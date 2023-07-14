0ef0d17361b99144d1cd2cca9811720dda7fcd91
u5man-ali <s_usman_ali@outlook.com>
2023-07-14 12:11:26 +0500

    my first commit with push

--- CS50/week1/half.c
+++ CS50/week1/half.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+
+
 float half(float bill, float tax, float tip);
 
 int main(void)
@@ -5,4 +9,15 @@ int main(void)
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sales Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
+
+    printf("$%.2f \n", half(bill_amount, tax_percent, tip_percent));
+}
+
+float half(float bill, float tax, float tip)
+{
+    float payment;
+    tax = (tax/100)*bill;
+    tip = (tip/100)*(bill+tax);
+    payment = (bill + tax + tip)/2;
+    return payment;
 }

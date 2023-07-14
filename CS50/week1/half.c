85b42f55846c0e87a0b5e4ffd3314f0295460cf1
Usman Ali <127815926+u5man-ali@users.noreply.github.com>
2023-07-14 09:29:36 +0000

    added practice files in week1

--- CS50/week1/half.c
+++ CS50/week1/half.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <cs50.h>
 
 
-float half(float bill, float tax, float tip);
+float half(float bill, float tax, float tip, int friends);
 
 int main(void)
 {
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sales Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
+    int friends = get_int("How many friends? ");
 
-    printf("$%.2f \n", half(bill_amount, tax_percent, tip_percent));
+    printf("Payment for each friend is $%.2f \n", half(bill_amount, tax_percent, tip_percent, friends));
 }
 
-float half(float bill, float tax, float tip)
+float half(float bill, float tax, float tip, int friends)
 {
     float payment;
     tax = (tax/100)*bill;
     tip = (tip/100)*(bill+tax);
-    payment = (bill + tax + tip)/2;
+    payment = (bill + tax + tip)/friends;
     return payment;
 }
